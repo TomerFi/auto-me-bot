175d2905a722e27e259e349c77a8fe1ed06cd413
Tomer Figenblat <tomer.figenblat@gmail.com>
2022-04-26 10:44:14 +0300

    docs: better code comments
    Signed-off-by: Tomer Figenblat <tomer.figenblat@gmail.com>

--- src/handlers/enforce-conventional-commits.js
+++ src/handlers/enforce-conventional-commits.js
@@ -11,10 +11,12 @@ const BOT_URL = 'https://github.com/TomerFi/auto-me-bot';
 module.exports = enforceConventionalCommits;
 
 /**
+ * Handler for verifying commit messages as conventional
  * @param {import Context from 'probot'} context
  */
 async function enforceConventionalCommits(context) {
     let startedAt = new Date().toISOString();
+    // create the initial check run and mark it as in_progress
     let checkRun = await context.octokit.checks.create(context.repo({
         head_sha: context.payload.pull_request.head.sha,
         name: CHECK_NAME,
@@ -22,60 +24,64 @@ async function enforceConventionalCommits(context) {
         started_at: startedAt,
         status: 'in_progress'
     }));
-
+    // get the commits associated with the PR
     let commitObjs = await context.octokit.rest.pulls.listCommits(context.repo({
         pull_number: context.payload.pull_request.number
     })).then(resp => resp.data);
-
-
+    // load the options
     let opts = await load(DEFAULT_CONFIG);
-
+    // get lint status for every commit
     let lintStatuses = await commitObjs.map(async commitObj => {
         return {
             commits_url: commitObj.html_url,
             report: await lint(commitObj.commit.message, opts.rules, opts.parserPreset ? {parserOpts: opts.parserPreset.parserOpts} : {})
         };
     });
-
+    // list warning and error statuses
     let errorStatuses = [];
     let warningStatuses = [];
     await Promise.all(lintStatuses).then(statuses => statuses.forEach(status => {
         if(!status.report.valid) {
-            errorStatuses.push(status);
+            errorStatuses.push(status); // the lint status is not valid
         } else if (status.report.warnings.length > 0) {
-            warningStatuses.push(status);
+            warningStatuses.push(status); // the lint status is valid but has warnings
         }
     }));
-
+    // default output for successful lint
     let finalConclusion = 'success';
     let outputReport = {
         title: 'Good Job!',
         summary: 'Nothing to do here, no one told me you\'re a commit-message-master'
     };
-
+    // check of error and warning
     let numError = errorStatuses.length;
     let numWarnings = warningStatuses.length;
     if (numError > 0) {
+        // found errors
         finalConclusion = 'failure';
         let reportSummary;
         if (numWarnings > 0) {
+            // found errors and warnings
             reportSummary = `Oops, looks like we got ${numError} errors, and ${numWarnings} warnings`;
         } else {
+            // found only errors - no warnings
             reportSummary = `Oops, looks like we got ${numError} non-conventional commit message${numError > 1 ? 's' : ''}`;
         }
+        // create output for error/error+warning
         outputReport = {
             title: 'Linting Failed',
             summary: reportSummary,
             text: errorStatuses.concat(warningStatuses).map(lintSts => parseLintStatus(lintSts)).join('\r\n')
         };
     } else if (numWarnings > 0) {
+        // found only warning - no errors
         outputReport = {
             title: 'Linting Found Warnings',
             summary: `Hmmm... we got ${numWarnings} warning${numWarnings > 1 ? 's' : ''} you might want to look at`,
             text: warningStatuses.map(lintSts => parseLintStatus(lintSts)).join('\r\n')
         };
     }
-
+    // update check run and mark it as completed
     await context.octokit.checks.update(context.repo({
         check_run_id: checkRun.data.id,
         name: CHECK_NAME,
@@ -88,13 +94,16 @@ async function enforceConventionalCommits(context) {
     }));
 }
 
+// create markdown segments for aggregating the lint status report
 function parseLintStatus(lintStatus) {
+    // create title from commit msg and link
     let statusLines = [
         `### ${lintStatus.commits_url}`,
         '```',
         lintStatus.report.input,
         '```'
     ];
+    // add warning table if warnings exist
     if (lintStatus.report.warnings.length > 0) {
         statusLines.push(
             '#### Warnings',
@@ -105,6 +114,7 @@ function parseLintStatus(lintStatus) {
             statusLines.push(`| ${w.name} | ${w.level} | ${w.message} |`);
         });
     }
+    // add error table if errors exist
     if (lintStatus.report.errors.length > 0) {
         statusLines.push(
             '#### Errors',
@@ -115,6 +125,6 @@ function parseLintStatus(lintStatus) {
             statusLines.push(`| ${e.name} | ${e.level} | ${e.message} |`);
         });
     }
-
+    // return output as string
     return statusLines.join('\r\n');
 }
